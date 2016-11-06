4b5bc1359984c639ebd0f14cb025baae69756318
Les Fletcher <les.fletcher@gmail.com>
2016-11-06 13:49:59 -0800

    basic emoji translations

--- server/fellow-kids/lib/translator.rb
+++ server/fellow-kids/lib/translator.rb
@@ -4,10 +4,12 @@ class Translator
     @from = from_lang
     @to = to_lang
     @replacement_data = self.class.read_replacement_data(@from, @to)
+    @emoji_data = self.class.read_emoji_data(@from, @to)
   end
 
   def translate(str)
     str = simple_replacements(str)
+    str = emoji_replacements(str)
 
     str
   end
@@ -22,6 +24,21 @@ class Translator
         value
       end
       str.gsub!(/\b#{key}\b/i, sub_val)
+    end
+
+    str
+  end
+
+  def emoji_replacements(str)
+    return str if @replacement_data.empty?
+
+    @emoji_data.each_pair do |key, value|
+      sub_val = if value.is_a?(Array)
+        value.sample
+      else
+        value
+      end
+      str.gsub!(/#{key}/, sub_val)
     end
 
     str
@@ -51,6 +68,12 @@ class Translator
       filename = "#{Rails.root}/translations/#{from_lang}/#{to_lang}/replacements.yml"
       file_data = File.file?(filename) ? YAML::load(File.open(filename)) : {}
       file_data['replacements']
+    end
+
+    def read_emoji_data(from_lang, to_lang)
+      filename = "#{Rails.root}/translations/#{from_lang}/#{to_lang}/emoji.yml"
+      file_data = File.file?(filename) ? YAML::load(File.open(filename)) : {}
+      file_data['emoji']
     end
 
   end
