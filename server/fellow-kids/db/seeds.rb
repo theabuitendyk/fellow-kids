14e06af506e12f397fc4bb99cbb8994cbd66247a
Les Fletcher <les.fletcher@gmail.com>
2016-11-06 11:59:04 -0800

    update seeds file

--- server/fellow-kids/db/seeds.rb
+++ server/fellow-kids/db/seeds.rb
@@ -16,29 +16,50 @@ grandpa_joe = User.create(name: 'Grandpa Joe', username: 'grandpa_joe', birth_ye
 
 convo1 = Conversation.create
 
-Message.create(user: thea, conversation: convo1, original_type: :old, old_translation: 'sorry about that, talk to you later')
-Message.create(user: owen, conversation: convo1, original_type: :youth, youth_translation: 'you should be, that was hella whack')
-Message.create(user: owen, conversation: convo1, original_type: :youth, youth_translation: 'we\'ll figure it out tomorrow')
-Message.create(user: thea, conversation: convo1, original_type: :old, old_translation: 'text you?')
-Message.create(user: owen, conversation: convo1, original_type: :youth, youth_translation: 'yas')
-Message.create(user: thea, conversation: convo1, original_type: :old, old_translation: 'ok')
+created_time = Time.zone.now
+
+Message.create(user: thea, created_at: created_time, updated_at: created_time, conversation: convo1, original_type: :old, old_translation: 'sorry about that, talk to you later')
+created_time += 1.second
+Message.create(user: owen, created_at: created_time, updated_at: created_time, conversation: convo1, original_type: :youth, youth_translation: 'you should be, that was hella whack')
+created_time += 1.second
+Message.create(user: owen, created_at: created_time, updated_at: created_time, conversation: convo1, original_type: :youth, youth_translation: 'we\'ll figure it out tomorrow')
+created_time += 1.second
+Message.create(user: thea, created_at: created_time, updated_at: created_time, conversation: convo1, original_type: :old, old_translation: 'text you?')
+created_time += 1.second
+Message.create(user: owen, created_at: created_time, updated_at: created_time, conversation: convo1, original_type: :youth, youth_translation: 'yas')
+created_time += 1.second
+Message.create(user: thea, created_at: created_time, updated_at: created_time, conversation: convo1, original_type: :old, old_translation: 'ok')
+created_time += 1.second
 
 convo2 = Conversation.create
 
-Message.create(user: grandpa_joe, conversation: convo2, original_type: :old, old_translation: 'Are you cool?')
-Message.create(user: timmy, conversation: convo2, original_type: :youth, youth_translation: 'i am hyphie af')
-Message.create(user: grandpa_joe, conversation: convo2, original_type: :old, old_translation: 'Timothy!')
-Message.create(user: grandpa_joe, conversation: convo2, original_type: :old, old_translation: 'Language!!!')
-Message.create(user: timmy, conversation: convo2, original_type: :youth, youth_translation: 'wat?')
-Message.create(user: timmy, conversation: convo2, original_type: :youth, youth_translation: 'i\'m just sayin\'')
+Message.create(user: grandpa_joe, created_at: created_time, updated_at: created_time, conversation: convo2, original_type: :old, old_translation: 'Are you cool?')
+created_time += 1.second
+Message.create(user: timmy, created_at: created_time, updated_at: created_time, conversation: convo2, original_type: :youth, youth_translation: 'i am hyphie af')
+created_time += 1.second
+Message.create(user: grandpa_joe, created_at: created_time, updated_at: created_time, conversation: convo2, original_type: :old, old_translation: 'Timothy!')
+created_time += 1.second
+Message.create(user: grandpa_joe, created_at: created_time, updated_at: created_time, conversation: convo2, original_type: :old, old_translation: 'Language!!!')
+created_time += 1.second
+Message.create(user: timmy, created_at: created_time, updated_at: created_time, conversation: convo2, original_type: :youth, youth_translation: 'wat?')
+created_time += 1.second
+Message.create(user: timmy, created_at: created_time, updated_at: created_time, conversation: convo2, original_type: :youth, youth_translation: 'i\'m just sayin\'')
+created_time += 1.second
 
 convo3 = Conversation.create
 
-Message.create(user: thea, conversation: convo3, original_type: :old, old_translation: 'Words Words')
-Message.create(user: les, conversation: convo3, original_type: :old, old_translation: 'Things Things')
-Message.create(user: galina, conversation: convo3, original_type: :old, old_translation: 'Stuff Stuff')
-Message.create(user: les, conversation: convo3, original_type: :old, old_translation: 'Junk Junk')
-Message.create(user: galina, conversation: convo3, original_type: :old, old_translation: 'Huh Huh')
-Message.create(user: thea, conversation: convo3, original_type: :old, old_translation: 'Yup Yup')
+Message.create(user: thea, created_at: created_time, updated_at: created_time, conversation: convo3, original_type: :old, old_translation: 'Words Words')
+created_time += 1.second
+Message.create(user: les, created_at: created_time, updated_at: created_time, conversation: convo3, original_type: :old, old_translation: 'Things Things')
+created_time += 1.second
+Message.create(user: galina, created_at: created_time, updated_at: created_time, conversation: convo3, original_type: :old, old_translation: 'Stuff Stuff')
+created_time += 1.second
+Message.create(user: les, created_at: created_time, updated_at: created_time, conversation: convo3, original_type: :old, old_translation: 'Junk Junk')
+created_time += 1.second
+Message.create(user: galina, created_at: created_time, updated_at: created_time, conversation: convo3, original_type: :old, old_translation: 'Huh Huh')
+created_time += 1.second
+Message.create(user: thea, created_at: created_time, updated_at: created_time, conversation: convo3, original_type: :old, old_translation: 'Yup Yup')
+
+# Translate
 
 Message.all.each { |m| m.translate }
