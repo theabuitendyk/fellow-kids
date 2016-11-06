3f33982316028c323ec062c7967d71f47473c423
galinaskaya <galina.skaya@gmail.com>
2016-11-05 21:54:05 -0700

    Add some routes and controller actions

--- server/fellow-kids/app/controllers/conversations_controller.rb
+++ server/fellow-kids/app/controllers/conversations_controller.rb
@@ -1,6 +1,25 @@
 class ConversationsController < ApplicationController
+  respond_to :json
 
   def index
+    @conversations = current_user.conversations
 
+    render json: @conversations, each_serializer: conversation_serializer, root: "conversations"
   end
+
+  def show
+    render json: @conversation, serializer: conversation_serializer, root: "conversation"
+  end
+
+  def create
+    @conversation = current_user.conversations.create
+    render json: @conversation, serializer: conversation_serializer, root: "conversation"
+  end
+
+  private
+
+  def conversation_serializer
+    ConversationSerializer
+  end
+
 end
--- server/fellow-kids/config/routes.rb
+++ server/fellow-kids/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
+
+  resources :users
+  resources :conversations
+  resources :messages
+
 end
